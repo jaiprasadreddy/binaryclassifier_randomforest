c98cc7dbd9b68f8f5cdd0cc2b457706e272aeb6e
Manoj <jp.rampure@gmail.com>
2016-11-07 12:42:54 +0530

    first commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# General Dependencies
+sudo apt-get install build-essential python-dev -y
+
+# Install required softwares
+pip install numpy -y
+pip install pandas -y
+pip install matplotlib -y
+pip install -U scikit-learn -y
+pip install costcla -y
